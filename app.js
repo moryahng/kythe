a1e290f05140779c28ca8ce4258261529699ec02
Moryah Ng <moryah.ng@gmail.com>
2023-03-18 00:19:04 +0800

    18Mar

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-var clientHeight = document.getElementById('container').clientHeight;
+var heroheight = document.getElementById('herobanner').clientHeight;
+
+window.onscroll = function (event) {
+    var scroll = window.pageYOffset;
+    if (scroll > heroheight) {
+        document.getElementById("navbar_white").style.display = "none";
+        document.getElementById("navbar").style.display = "block";
+    }
+else{
+    document.getElementById("navbar_white").style.display = "block";
+    document.getElementById("navbar").style.display = "none";  
+}}
